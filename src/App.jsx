3b5d3973e25fba32754d08a3ba62b1ed59e0037c
mari0-0 <abhainike@gmail.com>
2025-03-04 00:08:46 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,74 @@
-"use client";
-
-import { useState, useRef, useEffect } from "react";
-import { Bot, Percent, RefreshCw, X } from "lucide-react";
+import { useState } from "react";
+import { CircleDollarSign, Dices, Percent } from "lucide-react";
+import { ScrollArea, ScrollBar } from "./components/ui/scroll-area";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useBalance } from "wagmi";
 
 export default function App() {
-  const [betAmount, setBetAmount] = useState("0.00");
-  const [profitOnWin, setProfitOnWin] = useState("0.00");
-  const [sliderValue, setSliderValue] = useState(68.71);
-  const [mode, setMode] = useState("Manual");
-  const [isDragging, setIsDragging] = useState(false);
-
-  const multiplier = ((100 / (100 - sliderValue))).toFixed(4);
-  const rollOver = sliderValue.toFixed(2);
-  const winChance = (100 - sliderValue).toFixed(4);
-
-  const sliderTrackRef = useRef(null);
-
-  const handleSliderMouseDown = (e) => {
-    setIsDragging(true);
-    updateSliderValue(e);
-  };
-
-  const handleSliderMouseMove = (e) => {
-    if (isDragging) {
-      updateSliderValue(e);
-    }
-  };
-
-  const handleSliderMouseUp = () => {
-    setIsDragging(false);
-  };
-
-  const updateSliderValue = (e) => {
-    if (!sliderTrackRef.current) return;
-
-    const rect = sliderTrackRef.current.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const percentage = Math.min(Math.max((x / rect.width) * 100, 2), 98);
-    setSliderValue(percentage);
-  };
-
-  const smoothSliderValue = (value) => {
-    if (value > 50) {
-      return value - Math.pow((50 - value) / 50, 2) * 3;
-    } else {
-      return value + Math.pow((value - 50) / 50, 2) * 3;
-    }
-  };
-
-  useEffect(() => {
-    if (isDragging) {
-      window.addEventListener("mousemove", handleSliderMouseMove);
-      window.addEventListener("mouseup", handleSliderMouseUp);
-      window.addEventListener("touchmove", handleSliderMouseMove);
-      window.addEventListener("touchend", handleSliderMouseUp);
-    }
-
-    return () => {
-      window.removeEventListener("mousemove", handleSliderMouseMove);
-      window.removeEventListener("mouseup", handleSliderMouseUp);
-      window.removeEventListener("touchmove", handleSliderMouseMove);
-      window.removeEventListener("touchend", handleSliderMouseUp);
-    };
-  }, [isDragging, handleSliderMouseMove, handleSliderMouseUp]);
+  const account = useAccount();
+  const balanceObj = useBalance({
+    address: account.address,
+  });
+  const balance = parseFloat(Number(balanceObj.data.value) / 10 ** 9);
+  const [betAmount, setBetAmount] = useState(0);
+  const [profitOnWin, setProfitOnWin] = useState(0);
 
   return (
     <div className="flex flex-col md:flex-row w-full min-h-screen bg-slate-900 text-white">
       {/* Left Panel */}
       <div className="w-full md:w-[400px] p-6 bg-slate-800 border-r border-slate-700">
-        {/* Mode Toggle */}
-        <div className="bg-slate-900 rounded-full p-2 flex mb-6 items-center gap-2">
-          <button
-            className={`flex-1 w-12 h-12 py-2 px-4 rounded-full text-center ${
-              mode === "Manual" ? "bg-slate-700 text-white" : "text-gray-400"
-            }`}
-            onClick={() => setMode("Manual")}
-          >
-            Manual
-          </button>
-          <button
-            className={`flex-1 w-12 h-12 py-2 px-4 rounded-full text-center hover:bg-slate-700 ${
-              mode === "Auto" ? "bg-slate-700 text-white" : "text-gray-400"
-            }`}
-            onClick={() => setMode("Auto")}
-          >
-            Auto
-          </button>
-          <button
-            className={`w-12 h-12 rounded-full text-center hover:bg-slate-700 flex justify-center items-center ${
-              mode === "Strategy" ? "bg-slate-700 text-white" : "text-gray-400"
-            }`}
-            onClick={() => setMode("Strategy")}
-          >
-            <Bot size={20} />
-          </button>
+        <h1 className="mb-6 font-bold text-2xl flex items-center justify-center gap-1">
+          <Dices /> DICE GAME
+        </h1>
+
+        <div className="mb-6">
+          <div className="flex justify-between mb-2">
+            <label className="text-gray-400">Blockchain</label>
+          </div>
+          <div className="flex pr-4 bg-slate-900 rounded-md border border-slate-600">
+            <select className="w-full px-3 py-3 rounded-md outline-none  text-white bg-slate-900">
+              <option className="bg-slate-800 text-white" value="option1">
+                Sepolia
+              </option>
+              <option className="bg-slate-800 text-white" value="option2">
+                Gorelia
+              </option>
+              <option className="bg-slate-800 text-white" value="option3">
+                Polygon
+              </option>
+            </select>
+          </div>
         </div>
 
         {/* Bet Amount */}
         <div className="mb-6">
           <div className="flex justify-between mb-2">
             <label className="text-gray-400">Bet Amount</label>
-            <span className="text-gray-400">₹0.00</span>
+            <span className="text-gray-400 flex gap-1 items-center">
+              <CircleDollarSign size={20} /> {balance.toFixed(2)} Gwei
+            </span>
           </div>
           <div className="flex">
             <div className="flex-1 bg-slate-900 rounded-l-md border border-slate-700 flex items-center">
               <input
                 type="number"
+                min={0}
+                max={balance}
                 value={betAmount}
-                onChange={(e) => setBetAmount(e.target.value)}
+                onChange={(e) => {
+                  if (e.target.value < balance) {
+                    setBetAmount(e.target.value);
+                    setProfitOnWin(e.target.value * 2);
+                  }
+                }}
                 className="w-full bg-transparent px-4 py-3 outline-none"
               />
               <div className="px-2">
                 <span className="flex items-center justify-center w-6 h-6 bg-yellow-500 rounded-full text-black text-xs">
-                  ₹
+                  <CircleDollarSign size={20} />
                 </span>
               </div>
             </div>
-            <button className="px-4 py-3 bg-slate-700 border-t border-b border-slate-600">
-              ½
-            </button>
             <button className="px-4 py-3 bg-slate-700 rounded-r-md border border-slate-600">
               2×
             </button>
@@ -129,7 +79,6 @@ export default function App() {
         <div className="mb-6">
           <div className="flex justify-between mb-2">
             <label className="text-gray-400">Profit on Win</label>
-            <span className="text-gray-400">₹0.00</span>
           </div>
           <div className="flex bg-slate-700 rounded-md border border-slate-600    ">
             <input
@@ -140,20 +89,63 @@ export default function App() {
             />
             <div className="px-4 flex items-center">
               <span className="flex items-center justify-center w-6 h-6 bg-yellow-500 rounded-full text-black text-xs">
-                ₹
+                <CircleDollarSign size={20} />
               </span>
             </div>
           </div>
         </div>
 
         {/* Bet Button */}
-        <button className="w-full py-4 bg-[#5cff5c] hover:bg-[#4eee4e] text-black font-medium rounded-md">
+        <button
+          className={`w-full py-4 ${
+            account.isConnecting ||
+            account.isDisconnected ||
+            account.isReconnecting
+              ? "bg-green-600"
+              : "bg-green-500 cursor-pointer"
+          } hover:bg-green-600 text-black font-medium rounded-md transition-all`}
+          disabled={
+            account.isConnecting ||
+            account.isDisconnected ||
+            account.isReconnecting
+          }
+        >
           Bet
         </button>
+
+        <div className="mt-12 w-full flex flex-col gap-6">
+          <div className="w-full flex flex-col gap-2">
+            <div className="w-full flex gap-2 items-center ">
+              <div className="w-4 h-4 rounded-sm bg-green-500" />
+              <p className="font-semibold text-slate-300">Wins : 12</p>
+            </div>
+            <div className="w-full flex gap-2 items-center ">
+              <div className="w-4 h-4 rounded-sm bg-red-500" />
+              <p className="font-semibold text-slate-300">Loses : 12</p>
+            </div>
+          </div>
+
+          <div className="w-full">
+            <h4 className="mb-3 font-semibold text-lg text-slate-300">
+              History
+            </h4>
+            <ScrollArea className="w-full h-36 ">
+              <div className="flex flex-wrap gap-2 content-start">
+                {Array.from({ length: 140 }).map((_, i) => (
+                  <div key={i} className="w-4 h-4 rounded-sm bg-green-500" />
+                ))}
+              </div>
+              <ScrollBar className="bg-slate-700" />
+            </ScrollArea>
+          </div>
+        </div>
       </div>
 
       {/* Right Panel */}
       <div className="flex-1 p-6 flex flex-col">
+        <div className="w-full flex justify-end">
+          <ConnectButton />
+        </div>
         {/* Slider Section */}
         <div className="flex-1 flex flex-col justify-center items-center">
           <div className="relative p-3 w-10/12 bg-slate-600 rounded-full">
@@ -165,32 +157,40 @@ export default function App() {
                           border-l-[10px] border-l-transparent 
                           border-r-[10px] border-r-transparent 
                           border-b-[10px] border-b-slate-600
-                          after:content-['0'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
+                          after:content-['1'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
                           "
                 ></div>
                 <div
-                  className=" absolute top-[-20px] right-3/4 translate-x-3/4 w-0 h-0 
+                  className=" absolute top-[-20px] right-5/6 translate-x-5/6 w-0 h-0 
                           border-l-[10px] border-l-transparent 
                           border-r-[10px] border-r-transparent 
                           border-b-[10px] border-b-slate-600
-                          after:content-['25'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
+                          after:content-['2'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
                           "
                 ></div>
 
                 <div
-                  className=" absolute top-[-20px] right-1/2 translate-x-1/2 w-0 h-0 
+                  className=" absolute top-[-20px] right-4/6 translate-x-4/6 w-0 h-0 
                           border-l-[10px] border-l-transparent 
                           border-r-[10px] border-r-transparent 
                           border-b-[10px] border-b-slate-600
-                          after:content-['50'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
+                          after:content-['3'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
                           "
                 ></div>
                 <div
-                  className=" absolute top-[-20px] right-1/4 translate-x-2/5 w-0 h-0 
+                  className=" absolute top-[-20px] right-2/6 translate-x-3/6 w-0 h-0 
                           border-l-[10px] border-l-transparent 
                           border-r-[10px] border-r-transparent 
                           border-b-[10px] border-b-slate-600
-                          after:content-['75'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
+                          after:content-['4'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
+                          "
+                ></div>
+                <div
+                  className=" absolute top-[-20px] right-1/6 -translate-x-2/6 w-0 h-0 
+                          border-l-[10px] border-l-transparent 
+                          border-r-[10px] border-r-transparent 
+                          border-b-[10px] border-b-slate-600
+                          after:content-['5'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
                           "
                 ></div>
                 <div
@@ -198,90 +198,37 @@ export default function App() {
                           border-l-[10px] border-l-transparent 
                           border-r-[10px] border-r-transparent 
                           border-b-[10px] border-b-slate-600
-                          after:content-['100'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
+                          after:content-['6'] after:font-semibold after:absolute after:-top-6 after:left-1/2 after:-translate-x-1/2
                           "
                 ></div>
               </>
-            
+
               {/* Slider Track */}
-              <div
-                ref={sliderTrackRef}
-                className="p-3 bg-[#2a3a4a] overflow-hidden relative rounded-xl cursor-pointer flex justify-center items-center"
-                onMouseDown={handleSliderMouseDown}
-                onTouchStart={handleSliderMouseDown}
-              >
+              <div className="p-3 bg-[#2a3a4a] overflow-hidden relative rounded-xl cursor-pointer flex justify-center items-center">
                 <input
                   type="range"
                   min="0"
                   max="100"
-                  value={sliderValue}
                   className="w-full h-2 rounded-lg appearance-none cursor-pointer outline-none transition-all"
                   style={{
-                    background: `linear-gradient(to right, #E9113C ${sliderValue}%, #00E701 ${sliderValue}%)`,
+                    background: `linear-gradient(to right, #E9113C 50%, #00E701 50%)`,
                   }}
+                  disabled
                 />
               </div>
             </div>
             {/* Slider Thumb */}
-            <div
-              className="absolute top-1/2 -translate-y-1/2 w-8 h-8 lg:h-10 lg:w-10 bg-blue-400 flex items-center justify-center cursor-pointer rounded-sm select-none"
-              style={{
-                left: `calc(${smoothSliderValue(sliderValue)}% - 20px)`,
-              }}
-              onMouseDown={(e) => {
-                e.stopPropagation();
-                setIsDragging(true);
-              }}
-              onTouchStart={(e) => {
-                e.stopPropagation();
-                setIsDragging(true);
-              }}
-            >
-              <div className="text-sky-600 font-bold">|||</div>
-            </div>
           </div>
         </div>
 
         {/* Bottom Controls */}
         <div className="p-4 pt-3 grid grid-cols-3 gap-4 mt-auto bg-slate-800 rounded-sm">
           <div>
-            <label className="block text-gray-400 mb-2">Multiplier</label>
-            <div className="flex bg-slate-900 rounded-md border border-slate-700">
-              <input
-                type="text"
-                value={multiplier}
-                className="w-full bg-transparent px-4 py-3 outline-none"
-                readOnly
-              />
-              <div className="px-4 flex items-center">
-                <span className="text-gray-400">
-                  <X size={20} className="text-gray-400" />
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <label className="block text-gray-400 mb-2">Roll Over</label>
-            <div className="flex bg-slate-900 rounded-md border border-slate-700">
-              <input
-                type="text"
-                value={rollOver}
-                className="w-full bg-transparent px-4 py-3 outline-none"
-                readOnly
-              />
-              <div className="px-4 flex items-center">
-                <RefreshCw size={20} className="text-gray-400" />
-              </div>
-            </div>
-          </div>
-
-          <div>
             <label className="block text-gray-400 mb-2">Win Chance</label>
             <div className="flex bg-slate-900 rounded-md border border-slate-700">
               <input
                 type="text"
-                value={winChance}
+                value={90}
                 className="w-full bg-transparent px-4 py-3 outline-none"
                 readOnly
               />
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,39 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./components/theme-provider";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import {
+  baseSepolia,
+  optimismSepolia,
+  arbitrumSepolia,
+  polygonAmoy,
+  sepolia,
+} from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [sepolia, polygonAmoy, optimismSepolia, arbitrumSepolia, baseSepolia],
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider theme={darkTheme()} initialChain={sepolia}>
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <App />
+          </ThemeProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>
 );
